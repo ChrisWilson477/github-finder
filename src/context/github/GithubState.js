bfc216bd3096cc8c00d478c95001e137e7c223eb
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-05-15 20:27:40 -0600

    Github context made and refactored

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,81 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+	SEARCH_USERS,
+	SET_LOADING,
+	SET_ALERT,
+	CLEAR_USERS,
+	GET_USER,
+	GET_REPOS,
+	REMOVE_ALERT,
+} from '../types.js';
+
+const GithubState = (props) => {
+	const initialState = {
+		users: [],
+		user: {},
+		repos: [],
+		loading: false,
+		alert: null,
+	};
+
+	const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+	//Search Github Users
+	const searchUsers = async (text) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({ type: SEARCH_USERS, payload: res.data.items });
+	};
+
+	//Get single User
+	const getUser = async (username) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({
+			type: GET_USER,
+			payload: res.data,
+		});
+	};
+
+	//Get Users Repos
+	const getUserRepos = async (username) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({ type: GET_REPOS, payload: res.data });
+	};
+
+	//Clear Users From state
+	const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
+	//Set Loading
+	const setLoading = () => dispatch({ type: SET_LOADING });
+
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				user: state.user,
+				repos: state.repos,
+				loading: state.loading,
+				alert: state.alert,
+				searchUsers,
+				clearUsers,
+				getUser,
+				getUserRepos,
+			}}
+		>
+			{props.children}
+		</GithubContext.Provider>
+	);
+};
+
+export default GithubState;
